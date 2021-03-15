4401eca8929d23e3c76edf7f8b3226dd1d861a1e
Ervizal Buana <ervizal14@gmail.com>
2021-03-15 21:23:36 +0700

    Pengerjaan Exercise

---
+++ I0320035_Soal1_Tugas3.py
@@ -0,0 +1,14 @@
+#Pembuatan list array
+Identitas_teman = ['Sasa','Rafli','Alam','Yuku','Funny','Zaneta','Sekar','Hani','Stefany','Bonang'] #Mengisi value dari list
+print("Identitas_teman[4]: ", Identitas_teman[4])                                                   #Mencetak output list index ke-4
+print("Identitas_teman[6]: ", Identitas_teman[6])                                                   #Mencetak output list index ke-6
+print("Identitas_teman[7]: ", Identitas_teman[7])                                                   #Mencetak output list index ke-7)
+#Mengganti isi dari
+Identitas_teman[3] = 'Ilham Fairuzzaman'                                                            #Mengganti value list di index ke-3
+Identitas_teman[5] = 'Zaki'                                                                         #Mengganti value list di index ke-5
+Identitas_teman[9] = 'Safri'                                                                        #Mengganti value list di index ke-9
+j = 0                                                                                               #Membuat nilai awal variabel untuk perulangan isi list
+for i in range (0,10):                                                                              #Perulangan dengan nilai i = 0 hingga i = 10 sehingga dapat melakukan print sebanyak 10 kali
+    print(Identitas_teman[j])                                                                       #Melakukan print list Identitas_teman dengan nilai index j = 0 hingga j = 10
+    j+=1                                                                                            #Melakukan increment value pada variabel j
+print(len(Identitas_teman))
---
+++ I0320035_Soal2_Tugas3.py
@@ -0,0 +1,13 @@
+#Pembuatan dictionary
+My_identity = {"Nama":"Ervizal Buana", "Hobi 1":"Berenang", "Hobi 2":"Bermain gitar", "Hobi 3":"Memanah",
+			   "Sosmed 1":"instagram @eb_putra", "Sosmed 2":"linkedin: Ervizal Buana", "Sosmed 3":"twitter: @awgledek",
+			   "Lagu favorit 1":"Westlife - If I Let You Go", "Lagu Favorit 2":"Travis Atreo - As Long As You Love Me",
+			   "Lagu favorit 3":"Maroon 5 - Memories", "Makanan favorit 1":"Chicken Steak", "Makanan favorit 2":"Nasi Goreng",
+			   "Makanan favorit 3":"Micin"}
+print(My_identity)                              #Menampilkan output dictionary
+My_identity["Hobi 1"] = "Travelling"            #Mengubah isi dari dictionary ["Hobi 1"]
+My_identity["Sosmed 2"] = "Zenly: vizaall"      #Mengubah isi dari dictionary ["Sosmed 2"]
+del My_identity["Makanan favorit 1"]            #Menghapus dictionary ["Makanan favorit 1"]
+del My_identity["Makanan favorit 2"]            #Menghapus dictionary ["Makanan favorit 2"]
+My_identity["Hobi 4"] = "Bersepeda"             #Menambahkan dictionary ["Hobi 4"]
+print(My_identity)                              #Menampilkan output dictionary yang baru
