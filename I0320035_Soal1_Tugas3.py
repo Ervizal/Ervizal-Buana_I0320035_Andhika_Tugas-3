12e05f8021a09cbb6d35781f518e36be027bb4b5
Ervizal Buana <ervizal14@gmail.com>
2021-03-17 14:45:12 +0700

    Pengerjaan Soal Tugas 3

--- I0320035_Soal1_Tugas3.py
+++ I0320035_Soal1_Tugas3.py
@@ -12,6 +12,6 @@ Identitas_teman.append("Attar")
 Identitas_teman.append("Bagus")
 j = 0                                                                                               #Membuat nilai awal variabel untuk perulangan isi list
 for i in range (0,12):                                                                              #Perulangan dengan nilai i = 0 hingga i = 10 sehingga dapat melakukan print sebanyak 10 kali
-    print(Identitas_teman[j])                                                                       #Melakukan print list Identitas_teman dengan nilai index j = 0 hingga j = 10
+    print("Isi indeks list ke","[" + str(j) + "]", "adalah", Identitas_teman[j])                                                                       #Melakukan print list Identitas_teman dengan nilai index j = 0 hingga j = 10
     j+=1                                                                                            #Melakukan increment value pada variabel j
-print(len(Identitas_teman))                                                                         #Mencetak panjang/jumlah indeks pada list setelah diubah
+print("Panjang indeks list adalah", len(Identitas_teman))                                                                         #Mencetak panjang/jumlah indeks pada list setelah diubah
